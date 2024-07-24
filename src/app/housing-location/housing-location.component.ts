510cb5e26e0be86c313b3a68cc13e2b450ec6080
sora64 <phelpsa64@gmail.com>
2024-07-24 17:26:06 -0400

    added input, separated styles out into their own files

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,41 +1,13 @@
-import { Component } from "@angular/core";
-
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HousingLocation } from "../housinglocation";
 @Component({
   selector: "app-housing-location",
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: ` <p>housing-location works!</p> `,
-  styles: `
-  .listing {
-  background: var(--accent-color);
-  border-radius: 30px;
-  padding-bottom: 30px;
-}
-.listing-heading {
-  color: var(--primary-color);
-  padding: 10px 20px 0 20px;
-}
-.listing-photo {
-  height: 250px;
-  width: 100%;
-  object-fit: cover;
-  border-radius: 30px 30px 0 0;
-}
-.listing-location {
-  padding: 10px 20px 20px 20px;
-}
-.listing-location::before {
-  content: url("/assets/location-pin.svg") / "";
-}
-section.listing a {
-  padding-left: 20px;
-  text-decoration: none;
-  color: var(--primary-color);
-}
-section.listing a::after {
-  content: "\\203A";
-  margin-left: 5px;
-}
-  `,
+  styleUrls: ["./housing-location.component.css"],
 })
-export class HousingLocationComponent {}
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
